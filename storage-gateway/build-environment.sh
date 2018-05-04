c9b9dcd6069af9a824a45d6be8b0a5669f16e3ea
Christopher Madison <chris.madison@levvel.io>
2018-05-04 13:30:38 -0400

    added new script for gw creation

--- storage-gateway/build-environment.sh
+++ storage-gateway/build-environment.sh
@@ -1,13 +1,9 @@
 
 usage () {
 	echo ""
-	echo "Usage: $0 -n <MasterStackName> -s <SimHostKeyPairName> -g <GatewayKeyPairName> [-S <SimHostRegion>] [-G <GatewayRegion>] [-b <BucketName>] [-p <AWSProfile>] "
+	echo "Usage: $0 -n <MasterStackName> -s <SimHostKeyPairName>  [-p <AWSProfile>] "
 	echo "  -n: Master stack name, should be short - 2-3 characters"
 	echo "  -s: EC2 Key Pair name to leverage for simulated host access"
-	echo "  -g: EC2 Key Pair name to leverage for storage gateway"
-	echo "  -S: Sim host region, default is us-west-2"
-	echo "  -G: Gateway region, default is us-east-2"
-	echo "  -b: S3 bucket name to use for working directory for CloudFormation"
 	echo "  -p: Default profile will be used unless you specify a different profile"
 	echo ""
 }
@@ -15,34 +11,24 @@ usage () {
 AWSPROFILE=
 BUCKET='levvel-cloud-build-bucket'
 MASTERSTACKNAME=
-SIMHOSTKEYNAME=
-GATEWAYKEYNAME=
-SIMHOSTREGION='us-west-2'
-GATEWAYREGION='us-east-2'
+SIMHOST_KEYNAME=
+SIMHOST_REGION='us-west-2'
+SIMHOST_GATEWAY_REGION='us-east-2'
+DRHOST_KEYNAME=
+DRHOST_REGION='us-west-1'
+DRHOST_GATEWAY_REGION='us-east-1'
 
-while getopts n:s:g:S:G:b:p: o
+while getopts n:s:p: o
 do
 	case $o in
 		n)
 			MASTERSTACKNAME=$OPTARG
 			;;
-		b)
-			BUCKET=$OPTARG
-			;;
 		s)
-			SIMHOSTKEYNAME=$OPTARG
-			;;
-		g)
-			GATEWAYKEYNAME=$OPTARG
+			SIMHOST_KEYNAME=$OPTARG
 			;;
 		p)
 			AWSPROFILE=$OPTARG
-			;;
-		S)
-			SIMHOSTREGION=$OPTARG
-			;;
-		G)
-			GATEWAYREGION=$OPTARG
 			;;
 		\?)
 			usage
@@ -59,28 +45,15 @@ do
 	esac
 done
 
-if [ ! "$MASTERSTACKNAME" ] || [ ! "$SIMHOSTKEYNAME" ] #|| [ ! "$GATEWAYKEYNAME" ]
+if [ ! "$MASTERSTACKNAME" ] || [ ! "$SIMHOST_KEYNAME" ]
 then
 	usage
 	exit -1
 fi
 
-aws ${AWSPROFILE:+--profile $AWSPROFILE}cloudformation package --template-file  master-stack.json --s3-bucket $BUCKET --output-template-file master-stack-packaged.json
-aws ${AWSPROFILE:+--profile $AWSPROFILE}s3 mv ./master-stack-packaged.json  s3://${BUCKET}/
-aws ${AWSPROFILE:+--profile $AWSPROFILE} --region $SIMHOSTREGION cloudformation create-stack --stack-name ${MASTERSTACKNAME}-StorageGatewayMaster --capabilities CAPABILITY_NAMED_IAM --template-url https://s3.amazonaws.com/${BUCKET}/master-stack-packaged.json --parameters ParameterKey=MasterStackName,ParameterValue=$MASTERSTACKNAME ParameterKey=EC2Key,ParameterValue=$SIMHOSTKEYNAME ParameterKey=GatewayRegion,ParameterValue=$GATEWAYREGION
-aws ${AWSPROFILE:+--profile $AWSPROFILE} --region $SIMHOSTREGION cloudformation wait stack-create-complete --stack-name ${MASTERSTACKNAME}-StorageGatewayMaster
-
-
-#aws ${AWSPROFILE:+--profile $AWSPROFILE}cloudformation package --template-file  simulated-host-stack.json --s3-bucket $BUCKET --output-template-file simulated-host-stack-packaged.json
-#aws ${AWSPROFILE:+--profile $AWSPROFILE}s3 mv ./simulated-host-stack-packaged.json  s3://${BUCKET}/
-#aws ${AWSPROFILE:+--profile $AWSPROFILE} --region $SIMHOSTREGION cloudformation create-stack --stack-name ${MASTERSTACKNAME}-SimulatedOnPremises --capabilities CAPABILITY_NAMED_IAM --template-url https://s3.amazonaws.com/${BUCKET}/simulated-host-stack-packaged.json --parameters ParameterKey=MasterStackName,ParameterValue=$MASTERSTACKNAME ParameterKey=EC2Key,ParameterValue=$SIMHOSTKEYNAME
-#aws ${AWSPROFILE:+--profile $AWSPROFILE} --region $SIMHOSTREGION cloudformation wait stack-create-complete --stack-name ${MASTERSTACKNAME}-SimulatedOnPremises
-
-#SECGROUP=`aws ec2 describe-security-groups --region us-west-1 --query 'SecurityGroups[?Tags[?Value==\`'"${MASTERSTACKNAME}"'-win1SecurityGroup\`]].GroupId' --output text`
-
-#aws ${AWSPROFILE:+--profile $AWSPROFILE}cloudformation package --template-file  storage-gateway-stack.json --s3-bucket $BUCKET --output-template-file storage-gateway-stack-packaged.json
-#aws ${AWSPROFILE:+--profile $AWSPROFILE}s3 mv ./storage-gateway-stack-packaged.json  s3://${BUCKET}/
-#aws ${AWSPROFILE:+--profile $AWSPROFILE} --region $GATEWAYREGION cloudformation create-stack --stack-name ${MASTERSTACKNAME}-GatewayStack --capabilities CAPABILITY_NAMED_IAM --template-url https://s3.amazonaws.com/${BUCKET}/storage-gateway-stack-packaged.json --parameters ParameterKey=MasterStackName,ParameterValue=$MASTERSTACKNAME ParameterKey=EC2Key,ParameterValue=$GATEWAYKEYNAME ParameterKey=SimHostSecurityGroup,ParameterValue=$SECGROUP  ParameterKey=SimHostInstance,ParameterValue=${MASTERSTACKNAME}-win1Instance
-#aws ${AWSPROFILE:+--profile $AWSPROFILE} --region $GATEWAYREGION cloudformation wait stack-create-complete --stack-name ${MASTERSTACKNAME}-GatewayStack
+aws ${AWSPROFILE:+--profile $AWSPROFILE}cloudformation package --template-file  simhost-master-stack.json --s3-bucket $BUCKET --output-template-file simhost-master-stack-packaged.json
+aws ${AWSPROFILE:+--profile $AWSPROFILE}s3 mv ./simhost-master-stack-packaged.json  s3://${BUCKET}/
+aws ${AWSPROFILE:+--profile $AWSPROFILE} --region $SIMHOST_REGION cloudformation create-stack --stack-name ${MASTERSTACKNAME}-StorageGatewayMaster --capabilities CAPABILITY_NAMED_IAM --template-url https://s3.amazonaws.com/${BUCKET}/simhost-master-stack-packaged.json --parameters ParameterKey=MasterStackName,ParameterValue=$MASTERSTACKNAME ParameterKey=SimHostKey,ParameterValue=$SIMHOST_KEYNAME ParameterKey=GatewayRegion,ParameterValue=$SIMHOST_GATEWAY_REGION
+aws ${AWSPROFILE:+--profile $AWSPROFILE} --region $SIMHOST_REGION cloudformation wait stack-create-complete --stack-name ${MASTERSTACKNAME}-StorageGatewayMaster
 
 echo done
